d82fae7bf4f0c520b88f44ec02649c1691701132
Habteab <keshi2017@gmail.com>
2022-05-18 16:10:42 -0700

    feat(splash screen animation): add splash screen animation component

---
+++ components/SplashScreenAnimation.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const SplashScreenAnimation = () => {
+  return (
+    <View>SplashScreenAnimation</View>
+  )
+}
+
+export default SplashScreenAnimation
