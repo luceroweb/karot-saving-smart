f652b02e38d5f050bd86107a2a2132c6f4f251dc
alberme <albertm@alphaworks.tech>
2022-05-23 15:47:12 -0700

    refactor(splashscreen): move SplashScreenAnimation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar as RNStatusBar } from "react-native";
 import SplashScreenAnimation from './Components/SplashScreenAnimation';
 import { LinearGradient } from 'expo-linear-gradient';
 interface LoggedIn {
@@ -11,35 +11,28 @@ interface LoggedIn {
 export default function App() {
   const [loggedIn, setLoggedIn] = useState<LoggedIn>({
     status: "",
-    screen: "login",
+    screen: "splash",
   });
   return (
-    <SplashScreenAnimation>
-      <View style={styles.container}>
-        <LinearGradient
-          colors={['#2383C9', '#5A1E70']}
-          start={{ x: 0.0, y: 0.0 }}
-          end={{ x: 1.8, y: 1.1 }}
-          style={styles.linearGradientContainer}
-        >
-        {loggedIn.screen === "login"
-          ? <Text style={{ fontWeight: 'bold', fontSize: 28 }}>Login screen</Text>
-          : <Text style={{ fontWeight: 'bold', fontSize: 28 }}>Overview</Text>}
-        <StatusBar style="auto" />
-        </LinearGradient>
-      </View>
-    </SplashScreenAnimation>
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#2383C9', '#5A1E70']}
+        start={{ x: 0.0, y: 0.0 }}
+        end={{ x: 1.8, y: 1.1 }}
+        style={StyleSheet.absoluteFill}
+      />
+      {loggedIn.screen === "splash" ? <SplashScreenAnimation setLoggedIn={setLoggedIn} />
+       : <Text style={{ fontWeight: 'bold', fontSize: 28 }}>Login screen</Text>}
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  linearGradientContainer: {
-    height: '100%',
-    width: '100%',
     alignItems: "center",
     justifyContent: "center",
-  }
+  },
 });
+
