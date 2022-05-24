0642fa249abf0717dde8e2d3b7c90adda9773b3a
Juan Lucero <jlucero@alhpaworks.tech>
2022-05-24 13:01:42 -0700

    feat(reduxtoolkit): add redux toolkit with accounts reducers

---
+++ Utils/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import accountReducer from "./accountSlice";
+
+export const store = configureStore({
+  reducer: {
+    account: accountReducer,
+  },
+});
