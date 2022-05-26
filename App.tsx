48805a36791366105830672eb635ba796031e671
Garet Hough <garetshough@gmail.com>
2022-05-26 14:24:39 -0700

    style(login): removed the linear gradient from app.tsx to login.js #12

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Login from "./Components/Login";
-import { LinearGradient } from "expo-linear-gradient";
 
 interface LoggedIn {
   status: string;
@@ -16,13 +15,8 @@ export default function App() {
   });
   return (
     <View style={styles.container}>
-      <LinearGradient
-      start={{x: 0, y: 0}} end={{x: 1.6, y: 1}} colors={['#2383C9', '#5A1E70']}
-        style={styles.container}
-      >
       <Login />
       <StatusBar style="auto" />
-      </LinearGradient>
     </View>
   );
 }
--- Components/Login.js
+++ Components/Login.js
@@ -10,6 +10,7 @@ import {
   Sarabun_400Regular,
   Sarabun_300Light,
 } from "@expo-google-fonts/sarabun";
+import { LinearGradient } from "expo-linear-gradient";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -40,21 +41,26 @@ const Login = () => {
 
   return (
     <View style={styles.container}>
-      <Image source={karotBunny} style={styles.bunnyLogo} />
-      <Image source={karotSlogan} style={styles.karotSlogan} />
-      <TouchableOpacity
-        disabled={!request}
-        color="white"
-        title="Login"
-        onPress={() => {
-          promptAsync();
-        }}
-        style={styles.loginButton}
+      <LinearGradient
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1.6, y: 1 }}
+        colors={["#2383C9", "#5A1E70"]}
+        style={styles.container}
       >
-        <Text style={styles.textLogin}>Login</Text>
-      </TouchableOpacity>
-      {/* This will deisplay wether u are logged in or not */}
-      <Text>{loggedIn === "success" ? "Logged In" : "Logged Out"}</Text>
+        <Image source={karotBunny} style={styles.bunnyLogo} />
+        <Image source={karotSlogan} style={styles.karotSlogan} />
+        <TouchableOpacity
+          disabled={!request}
+          color="white"
+          title="Login"
+          onPress={() => {
+            promptAsync();
+          }}
+          style={styles.loginButton}
+        >
+          <Text style={styles.textLogin}>Login</Text>
+        </TouchableOpacity>
+      </LinearGradient>
     </View>
   );
 };
@@ -65,6 +71,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     height: "100%",
+    width: "100%",
   },
   bunnyLogo: {
     width: 263,
@@ -87,7 +94,7 @@ const styles = StyleSheet.create({
   textLogin: {
     textAlign: "center",
     fontSize: 20,
-    fontFamily: 'Sarabun_700Bold'
+    fontFamily: "Sarabun_700Bold",
   },
 });
 
