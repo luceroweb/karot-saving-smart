dd15674e2b9674e14084f3cd91589b072fb359c2
Xavier Mercado <xmercado1576@gmail.com>
2022-05-27 15:55:15 -0700

    feat(expenseSlice): provide example for list type

--- Utils/expenseSlice.ts
+++ Utils/expenseSlice.ts
@@ -6,7 +6,16 @@ interface InitialStateType {
 }
 
 const initialState: InitialStateType = {
-  list: [],
+  list: [
+    // example expense data
+    // may want to change date type Date to String
+    // {
+    //   label: "Food",
+    //   saved: 237.51,
+    //   goal: 300,
+    //   date: new Date().toLocaleDateString("en-US", {month: "long", year: "numeric"}),
+    // },
+  ],
 };
 
 export const expenseSlice = createSlice({
