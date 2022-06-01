a12da653db78b3246d6423570ec7fe5cdb34d2e8
Tesfa-Worku <hopegorg@gmail.com>
2022-06-01 19:49:40 -0400

    fix(safe area view): add safeAreaView to app.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useState} from "react";
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  SafeAreaView,
+  StatusBar as RNStatusBar 
+} from "react-native";
 import SplashScreenAnimation from "./Components/SplashScreenAnimation";
 import { store } from "./Utils/store";
 import { Provider } from "react-redux";
@@ -19,26 +25,31 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        {/* Uncomment ReduxStateTest to test various state actions and reducers */}
-        {/* <ReduxStateTest /> */}
-        {/* <ExpensesForm /> */}
-        {loggedIn.screen === "splash" ? (
-          <SplashScreenAnimation setLoggedIn={setLoggedIn} />
-        ) : loggedIn.screen === "login" ? (
-          <Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-        ) : (
-          <Overview />
-        )}
-        <StatusBar style="auto" />        
-      </View>
+      <SafeAreaView style={styles.safeAreaContainer}>
+        <View style={styles.container}>
+          {/* Uncomment ReduxStateTest to test various state actions and reducers */}
+          {/* <ReduxStateTest /> */}
+          {/* <ExpensesForm /> */}
+          {loggedIn.screen === "splash" ? (
+            <SplashScreenAnimation setLoggedIn={setLoggedIn} />
+          ) : loggedIn.screen === "login" ? (
+            <Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+          ) : (
+            <Overview />
+          )}
+          <StatusBar style="auto" />        
+        </View>
+      </SafeAreaView>
     </Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreaContainer: {
     flex: 1,
+    paddingTop: RNStatusBar.currentHeight || 0
+  },
+  container: {
     alignItems: "center",
     justifyContent: "center",
     height: "100%",
