d6444695467a8bf3432d64da2dfd4f6cf3666cf2
Hiwot-Beshe <emulada23@gmail.com>
2022-06-08 16:23:46 -0700

    fix(profileIcon): add View

--- Components/ProfileIcon.tsx
+++ Components/ProfileIcon.tsx
@@ -1,15 +1,15 @@
-import { StyleSheet, TouchableOpacity, Image } from "react-native";
+import { StyleSheet,View, TouchableOpacity, Image } from "react-native";
 import { useSelector} from "react-redux";
 import { GlobalStateType } from "../Utils/types";
 
 function ProfileIcon() {
 	const userData = useSelector((state: GlobalStateType) => state.user.data);
 	return (
-		<>
+		<View>
 			<TouchableOpacity >
 				<Image source={{ uri: userData.avatar }} style={styles.profileImage} />
 			</TouchableOpacity>
-		</>
+		</View>
 	);
 }
 
