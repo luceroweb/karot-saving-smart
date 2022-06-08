b59b61c119bf4a4cc9ec698ef018ce05f4aac72c
Hiwot-Beshe <beshehiwot@gmail.com>
2022-06-08 15:08:37 -0700

    fix(profileIcon): remove view and  styles flex

--- Components/ProfileIcon.tsx
+++ Components/ProfileIcon.tsx
@@ -1,24 +1,19 @@
-import { View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { StyleSheet, TouchableOpacity, Image } from "react-native";
 import { useSelector} from "react-redux";
 import { GlobalStateType } from "../Utils/types";
 
 function ProfileIcon() {
 	const userData = useSelector((state: GlobalStateType) => state.user.data);
 	return (
-		<View style={styles.container}>
+		<>
 			<TouchableOpacity >
 				<Image source={{ uri: userData.avatar }} style={styles.profileImage} />
 			</TouchableOpacity>
-		</View>
+		</>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-        width: "100%",
-	},
 	profileImage: {
 		width: 30,
 		height: 30,
