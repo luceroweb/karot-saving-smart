4b5db34bbba709bf25f162002d23d31770413fd4
Xavier Mercado <xmercado1576@gmail.com>
2022-06-09 14:52:49 -0700

    fix(font): add 400regular font for budget card

--- Components/SplashScreenAnimation.tsx
+++ Components/SplashScreenAnimation.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState, useRef, FC } from 'react';
 import { Animated, Easing, StyleSheet, Image } from 'react-native';
-import {Sarabun_300Light,Sarabun_600SemiBold,Sarabun_700Bold,
+import {
+  Sarabun_300Light,
+  Sarabun_400Regular,
+  Sarabun_600SemiBold,
+  Sarabun_700Bold,
 } from "@expo-google-fonts/sarabun";
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
@@ -35,8 +39,10 @@ const SplashScreenAnimation: FC<Props> = ({ setLoggedIn }) => {
         await SplashScreen.preventAutoHideAsync();
         // preload any images, fonts, sounds, addtional assets
         await Font.loadAsync({Sarabun_300Light,
+          Sarabun_400Regular,
           Sarabun_600SemiBold,
-          Sarabun_700Bold,});
+          Sarabun_700Bold,
+        });
         await Asset.loadAsync([
           require("../Images/logo/logo_combined.png")
         ])
