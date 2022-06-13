e9942a00a01f35e72880fa55b4d6bd0c52f05457
Hiwot-Beshe <beshehiwot@gmail.com>
2022-06-13 08:18:51 -0700

    chore(ExpenseList): add helper function for ExpenseDetailView

--- Components/ExpenseList.tsx
+++ Components/ExpenseList.tsx
@@ -1,20 +1,33 @@
-import { View } from "react-native";
-import { useSelector} from "react-redux";
+import React, { useState } from "react";
+import { View, Pressable } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 import { GlobalStateType } from "../Utils/types";
 import ExpenseCard from "./ExpenseCard";
+import ExpenseDetailView from "./ExpenseDetailView";
+import { setSelectedId } from "../Utils/expenseSlice";
 
 const ExpenseList = () => {
-    const expenses = useSelector((state: GlobalStateType) => state.expenses.list);
-    const generateExpenses = expenses.map((expense, index) => (
-        <ExpenseCard expense={expense} key={index}/>
-        
-    ))
-  return (
-      <>
-      <View style={{flexDirection: "row", flexWrap: 'wrap'}}>
-      {generateExpenses}
-      </View>
-      </>
-  )
-}
-export default ExpenseList
+	const dispatch = useDispatch();
+	const [showModal, SetShowModal] = useState(false);
+	const expenses = useSelector((state: GlobalStateType) => state.expenses.list);
+	const generateExpenses = expenses.map((expense, index) => (
+		<Pressable
+			key={index}
+			onPress={() => {
+				SetShowModal(!showModal);
+				dispatch(setSelectedId(index));
+			}}
+		>
+			<ExpenseCard expense={expense} />
+		</Pressable>
+	));
+	return (
+		<>
+			{showModal && <ExpenseDetailView SetShowModal={SetShowModal} />}
+			<View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+				{generateExpenses}
+			</View>
+		</>
+	);
+};
+export default ExpenseList;
