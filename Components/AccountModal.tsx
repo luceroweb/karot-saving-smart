79a7ec803f55dad9eae5081f30c88123665828bb
eyobel7k <eyobel7k@gmail.com>
2022-06-14 14:39:16 -0700

    (AccountModal) :change variable declaration from var to let #65

--- Components/AccountModal.tsx
+++ Components/AccountModal.tsx
@@ -58,7 +58,7 @@ const AccountModal = memo<Props>(
     const onChanged = (text:any) => {
       let newText:any="";
       let numbers = '0123456789.';
-      for (var i=0; i < text.length; i++) {
+      for (let i=0; i < text.length; i++) {
           if(numbers.indexOf(text[i]) > -1 ) {
               newText = newText + text[i];
           }
