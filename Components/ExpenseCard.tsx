831bc7a4c6856167dbe3f475870ebe2ea5fa50d7
Xavier Mercado <xmercado1576@gmail.com>
2022-06-21 15:07:05 -0700

    fix(expense): adjust margin to fit two tiles #100

--- Components/ExpenseCard.tsx
+++ Components/ExpenseCard.tsx
@@ -40,7 +40,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     borderRadius: 23,
     aspectRatio: 1,
-    margin: 15,
+    margin: 8,
     height: 180,
   },
   expenseAmount: {
