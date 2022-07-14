1dbec7d811ae5c3458a6768ee068d883541c962f
Juan Lucero <jlucero@alhpaworks.tech>
2022-07-13 17:40:43 -0700

    style(expense dtails): simplified code and removed unused #127

--- Components/ExpenseDetailView.tsx
+++ Components/ExpenseDetailView.tsx
@@ -11,15 +11,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { GlobalStateType } from "../Utils/types";
 import { setSelectedId } from "../Utils/expenseSlice";
 import { LinearGradient } from "expo-linear-gradient";
-import {
-  useFonts,
-  Sarabun_700Bold,
-  Sarabun_400Regular,
-  Sarabun_300Light,
-} from "@expo-google-fonts/sarabun";
 import * as Progress from "react-native-progress";
 import { Feather } from "@expo/vector-icons";
-import Moment from "moment";
 import {
   setModalMode,
   setExpenseModalVisibility,
@@ -29,86 +22,62 @@ import {
 const { width } = Dimensions.get("window");
 
 export default function ExpenseDetailView() {
-  let [fontsLoaded] = useFonts({
-    Sarabun_700Bold,
-    Sarabun_400Regular,
-    Sarabun_300Light,
-  });
   const dispatch = useDispatch();
-  const selectedId = useSelector(
-    (state: GlobalStateType) => state.expenses.selectedId
-  );
-  const expenseLabel = useSelector(
-    (state: GlobalStateType) => state.expenses.list[selectedId].label
-  );
-  const goalDate = useSelector(
-    (state: GlobalStateType) => state.expenses.list[selectedId].date
-  );
-  const currentSaving = useSelector(
-    (state: GlobalStateType) => state.expenses.list[selectedId].saved
-  );
-  const targetAmount = useSelector(
-    (state: GlobalStateType) => state.expenses.list[selectedId].goal
-  );
-  const moneyTotal = useSelector(
-    (state: GlobalStateType) => state.budgets.remaining.accountsTotal
-  );
-  const milliseconds = goalDate;
-  const dateObject = new Date(milliseconds);
-  const forMatedDate = dateObject.toLocaleString("en-us", {
-    month: "short",
-    year: "numeric",
-  });
-  Moment.locale("en");
-  var dt = goalDate;
+
+  const expenses = useSelector((state: GlobalStateType) => state.expenses);
+
+  const expense = expenses.list[expenses.selectedId];
+
   return (
     <LinearGradient
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1.6, y: 1 }}
-        colors={["#2383C9", "#102745"]}
-        style={styles.linearGradient}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1.6, y: 1 }}
+      colors={["#2383C9", "#102745"]}
+      style={styles.linearGradient}
+    >
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(setExpenseModalVisibility(true));
+          dispatch(setModalMode("edit"));
+        }}
+        style={styles.expenseContainer}
       >
-        <TouchableOpacity
+        <Feather
+          style={styles.exitIcon}
+          name="x-circle"
+          size={24}
+          color="#FFFFFF"
           onPress={() => {
-            dispatch(setExpenseModalVisibility(true));
-            dispatch(setModalMode("edit"));
+            dispatch(setSelectedId(-1));
+            dispatch(setExpenseDetailsModalVisiblity(false));
           }}
-        >
-          <Feather
-            style={styles.exitIcon}
-            name="x-circle"
-            size={24}
-            color="#FFFFFF"
-            onPress={() => {
-              dispatch(setSelectedId(-1));
-              dispatch(setExpenseDetailsModalVisiblity(false));
-            }}
-          />
-          <Text style={styles.budgetTextTop}>{expenseLabel}</Text>
-          <Text style={styles.budgetText}>
-            {Platform.OS === "web"
-              ? forMatedDate
-              : Moment(dt).format("MMM YYYY")}
+        />
+        <Text style={styles.budgetTextTop}>{expense.label}</Text>
+        <Text style={styles.budgetText}>
+          {new Date(expense.date).toLocaleDateString()}
+        </Text>
+        <View style={styles.modal}>
+          <Text style={styles.savingText}>${expense.saved}</Text>
+          <Text style={styles.budgetTextBottom}>
+            ${expense.saved} of ${expense.goal}
           </Text>
-          <View style={styles.modal}>
-            <Text style={styles.savingText}>${currentSaving}</Text>
-            <Text style={styles.budgetTextBottom}>
-              ${currentSaving} of ${targetAmount}
-            </Text>
-            <View style={styles.bar}>
-              <Progress.Bar
-                progress={currentSaving / targetAmount || 0}
-                color="#05C473"
-                unfilledColor="#DBDBDB"
-                borderColor="#DBDBDB"
+          <View style={styles.bar}>
+            <Progress.Bar
+              progress={expense.saved / expense.goal || 0}
+              color="#05C473"
+              unfilledColor="#DBDBDB"
+              borderColor="#DBDBDB"
               width={width * 0.7 > 285 ? 285 : width * 0.7}
               height={10}
-              />
-            </View>
+            />
           </View>
-        </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
 
-        <View style={styles.transactionsExpenses}>
+      <View style={styles.transactionsExpenses}>
+        <Text style={styles.RecentTransactions}>Recent Transactions</Text>
+        <Text style={{ textAlign: "center", padding: 10 }}>none</Text>
+      </View>
     </LinearGradient>
   );
 }
@@ -121,11 +90,8 @@ const styles = StyleSheet.create({
     width: width - 30 > 380 ? 380 : width - 30,
     marginTop: Platform.OS === "web" ? "-35%" : "-30%",
   },
-  linearGradient: {
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 23,
-    alignSelf: "center",
+  expenseContainer: {
+    padding: 10,
   },
   budgetText: {
     textAlign: "center",
@@ -148,9 +114,10 @@ const styles = StyleSheet.create({
   },
   budgetTextBottom: {
     textAlign: "center",
-    fontSize: 10,
+    fontSize: 14,
     fontFamily: "Sarabun_300Light",
     padding: 10,
+    paddingBottom: 0,
   },
   toggleArrow: {
     marginLeft: 22,
@@ -177,8 +144,8 @@ const styles = StyleSheet.create({
   },
   RecentTransactions: {
     textAlign: "center",
+    padding: 10,
     fontSize: 17,
-    fontWeight: "bold",
     fontFamily: "Sarabun_400Regular",
     color: "#000000",
     backgroundColor: "#EEEEEE",
